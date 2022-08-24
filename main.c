37aa29d6c2c0b28ee8f1fdc91ba908cb94543b99
Zanchetta08 <110983552+Zanchetta08@users.noreply.github.com>
2022-08-24 09:40:41 -0300

    Add files via upload
    Criei a função de mediana, menor valor e maior valor

--- main.c
+++ main.c
@@ -2,13 +2,38 @@
 #include <stdlib.h>
 #include <time.h>
 
+void menorValor(int *vetor){
+    int valor = vetor[0];
+    for(int i = 0; i < 1000; i++){
+        if(valor > vetor[i]){
+            valor = vetor[i];
+        }
+    }
+    printf("O menor valor desse vetor eh: %d\n", valor);
+}
+void maiorValor(int *vetor){
+    int valor = vetor[0];
+    for(int i = 0; i < 1000; i++){
+        if(valor < vetor[i]){
+            valor = vetor[i];
+        }
+    }
+    printf("O maior valor desse vetor eh: %d\n", valor);
+}
+
+void mediana(int *vetor){
+    float mediana = 0;
+    mediana = (vetor[499] + vetor[500]) / 2.0;
+    printf("A mediana desse vetor eh: %.2f\n", mediana);
+}
+
 void media(int *vetor){
     float media = 0;
     for(int i = 0; i < 1000; i++){
         media = media + vetor[i];
     }
     media = media / 1000;
-    printf("A media desse vetor eh: %.6f\n", media);
+    printf("A media desse vetor eh: %.2f\n", media);
 }
 
 void bubbleSort(int *vetor){
@@ -27,7 +52,7 @@ void bubbleSort(int *vetor){
 void preencheVet(int *vetor){
     srand( (unsigned)time(NULL) );
     for(int i = 0; i < 1000; i++){
-        vetor[i] = 1 + (rand() % 100);
+        vetor[i] = 1 + (rand() % 10000);
     }
 }
 
@@ -39,7 +64,9 @@ int main(){
         printf("%d\n", vetor[i]);
     }
     media(vetor);
-
+    mediana(vetor);
+    maiorValor(vetor);
+    menorValor(vetor);
 
     return 0;
 }
